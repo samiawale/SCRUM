f32fb12fb9e95040c7c3164f1e230b565596e147
SenpaiOfBakaKun <emilianocbm2002@gmail.com>
2024-02-26 16:56:54 +0100

    Add Database

--- Scrum/myproject/myapp/models.py
+++ Scrum/myproject/myapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class MyModel(models.Model):
-    id = models.CharField(max_length=100)
+class MyTree(models.Model):
+    name = models.CharField(max_length=100)
     tree_type = models.CharField(max_length=100)
+    color = models.CharField(max_length = 100)
     x_coordinate = models.FloatField()
     y_coordinate = models.FloatField()
 
