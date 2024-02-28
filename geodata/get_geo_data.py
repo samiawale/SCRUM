98e88a3d9dcdae9ab361566623ba0b7d2cd4965e
SenpaiOfBakaKun <emilianocbm2002@gmail.com>
2024-02-28 16:09:50 +0100

    finished framework

--- geodata/get_geo_data.py
+++ geodata/get_geo_data.py
@@ -6,8 +6,13 @@ file_name = 'geo_data.geojson'
 if os.path.exists(file_name):
     gdf = gpd.read_file(file_name)
 
-    print("File loaded successfully.")
-    print(gdf[['Gattung','gebiet','pflanzjahr','strasse','geometry']])
+    data = gdf[['Gattung','pflanzjahr','gebiet','strasse']]
+    data['Lat'] = gdf.geometry.y
+    data['Long'] = gdf.geometry.x
+    
+
+
+    print(data)
     
 else:
     print("File does not exist in the current directory.")
