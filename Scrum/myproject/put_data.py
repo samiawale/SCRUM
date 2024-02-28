deb32d2f924b0ebe988948001387b546f4f42c7e
SenpaiOfBakaKun <emilianocbm2002@gmail.com>
2024-02-28 17:33:01 +0100

    ahaha

--- Scrum/myproject/put_data.py
+++ Scrum/myproject/put_data.py
@@ -6,6 +6,28 @@ django.setup()
 
 from myapp.models import Tree
 
+import os
+import geopandas as gpd
+
+file_name = 'geo_data.geojson'
+
+if os.path.exists(file_name):
+    gdf = gpd.read_file(file_name)
+
+    data = gdf[['Gattung','pflanzjahr','gebiet','strasse']]
+    data['Lat'] = gdf.geometry.y
+    data['Long'] = gdf.geometry.x
+    
+
+    print(data)
+    
+else:
+    print("File does not exist in the current directory.")
+
+
+
+'''''
+
 Tree.objects.create(
     gattung = 'Baum',
     pflanzjahr = 2025,
@@ -14,3 +36,4 @@ Tree.objects.create(
     lat = 1,
     long = 1
 )
+'''''
